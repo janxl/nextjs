1be5fe7aed618caa9b6671d85d1abb23e0560a89
Luke Baay <lukeluke211@gmail.com>
2019-06-14 10:38:01 +1000

    Added styled components to the project for styling, implemented and example on the about page

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,15 @@
 import Layout from '../components/mylayout.js'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  color: #a7a9ac;
+`;
 
 export default function About() {
   return (
     <Layout>
+      <Title>Squelling Pig</Title>
       <p>This is the about page</p>
     </Layout>
   )
