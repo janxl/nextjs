4382e80464c1ca4a7a022c571f74dc9fc8064943
Kendell Patrice <kendell.patrice@weareyou.com>
2019-06-18 12:03:28 +1000

    changing over to styled components

--- components/styleregistry.js
+++ components/styleregistry.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import '../styles/squealingpig/styles.css'
 import pigs from '../styles/squealingpig/styles.css'
 import second from '../styles/secondsite/styles.css'
 
 export default function GetCss(siteId) {
-  switch (siteId) {
-    case '6aee88e2-0358-429a-b721-82dd6854c4a1':
-      return pigs
-    default:
-      return second
-  }
+  // switch (siteId) {
+  //   case '6aee88e2-0358-429a-b721-82dd6854c4a1':
+  //     require('../styles/squealingpig/styles.css')
+  //     break;
+  //   default:
+  //     require('../styles/squealingpig/styles.css')
+  //     break
+  // }
+  return ''
 }
