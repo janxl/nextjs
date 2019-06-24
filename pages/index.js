e872d13e09e10af50f745ebd30d8e60cd128e215
Kendell Patrice <kendell.patrice@weareyou.com>
2019-06-24 15:45:44 +1000

    stripped a few lines of code

--- pages/index.js
+++ pages/index.js
@@ -36,8 +36,8 @@ export default class Dyn extends React.Component {
     }
 
     // Create a route for the initial render of the page
-    if (page == null || page == '' || page == '/')
-      query = {page: '/'}
+    // if (page == null || page == '' || page == '/')
+    //   query = {page: '/'}
 
     // Url for Root of CMS Tree, returning all nodes
     const treeRootUrl = `https://c1.adis.ws/cms/content/query?query=%7b%22sys.iri%22:%22http://content.cms.amplience.net/${urlId}%22%7d&scope=tree&store=twe&fullBodyObject=true`
