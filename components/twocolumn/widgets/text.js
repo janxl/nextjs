79ef6c71f65f21eefdf03f0fa6e87c20a4bf4836
Luke Baay <lukeluke211@gmail.com>
2019-06-25 16:18:23 +1000

    Got the two col component to parse markdown to react

--- components/twocolumn/widgets/text.js
+++ components/twocolumn/widgets/text.js
@@ -1,8 +1,10 @@
+import ReactMarkdown from 'react-markdown'
+
 export default (props) => {
   const {header, text} = props
 
   return <div>
     <h2 className="c-two-col__heading">{header}</h2>
-    <div>{text}</div>
+    <ReactMarkdown source={text} />
   </div>
 }
