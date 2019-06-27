4016a034b92d670a6cfc11183b17bd26edc1734b
Kendell Patrice <kendell.patrice@weareyou.com>
2019-06-27 10:02:36 +1000

    Added Simple Text

--- components/simpletext.js
+++ components/simpletext.js
@@ -1,6 +1,7 @@
 export default (props) => {
   const { paragraph, intro } = props
 
+  // This component renders the Simple Text Amplience Content Type
   return (
     <div className="container c-simple-text">
       { <p className="c-simple-text__paragraph">{intro.values.find((itemText) => itemText['locale'] === props.siteLanguage).value}</p> }
